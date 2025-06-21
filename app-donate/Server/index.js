258bcc9ba240323a8bb0364c0cfd3f0dec4eca1b
Minh <minhhoangdev3108@gmail.com>
2025-06-22 01:38:29 +0700

    test api connect to mongodb ok

--- app-donate/Server/index.js
+++ app-donate/Server/index.js
@@ -2,6 +2,8 @@ const e = require("express");
 const express = require("express");
 require("dotenv").config();
 
+const User = require("./model/User");
+
 // tao server
 const server = express();
 server.use(express.json());
@@ -13,3 +15,16 @@ mongoose
     console.log("connect success");
   })
   .catch((error) => console.log(error));
+
+// test api
+server.get("/User", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+const PORT = process.env.PORT || 3001;
+const HOST = process.env.HOST || "localhost";
+
+server.listen(3001, "localhost", () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+});
